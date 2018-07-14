64be0db7c453d1daf38128b29696ddf19c52dffb
Zhang Yang <dimlight1998@gmail.com>
2018-07-14 19:53:51 +0800

    Bug fix.

--- assets/Scripts/Common.ts
+++ assets/Scripts/Common.ts
@@ -2,6 +2,6 @@ let LastScore: number = 0;
 export function GetLastScore(): number {
     return LastScore;
 }
-export function SetLastScore(newScore: number): number {
+export function SetLastScore(newScore: number): void {
     LastScore = newScore;
 }
